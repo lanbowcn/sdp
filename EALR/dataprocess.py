395f1c27029d1ef05460c8260a69dc746931430d
lanbowcn <lanbow@live.cn>
2018-09-13 19:48:28 +0800

    change

---
+++ EALR/dataprocess.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+import datetime
+from pandas import DataFrame
+from sklearn.linear_model import LinearRegression
+
+def readfiles(projects):
+    for project in projects:
+        foldpath = "../dataset2"
+        my_matrix = pd.read_csv(open(foldpath+'/'+project))
+        print(my_matrix.tail(5))
+    return my_matrix
+
+# def divedeTosubsets(data,folds,seed):
+#     # seed使得结果可复现，打乱现有顺序
+#     np.random.seed(seed)
+#     np.random.shuffle(data)
+#     #数据集切分(array_split不等量分割# )
+#     sub=np.array_split(data,folds)
+#     return sub
+
+if __name__ == '__main__':
+    projects=['jdt.csv']
+    data = readfiles(projects)
+    for str in data['commitdate']:
+        np.Series a = datetime.datetime.strptime(data['commitdate'],"%Y/%m/%d  %H:%M:%S")
+
