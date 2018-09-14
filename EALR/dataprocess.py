f8986208d798ce5f76a5f50a837f70c76ae6ee49
lanbowcn <lanbow@live.cn>
2018-09-14 17:14:27 +0800

    完成根据年月进行数据提取，并排列在list中。便于之后的数据合并。 后续问题： 如何将dataframe输入模型，以及如何对dataframe进行操作！

--- EALR/dataprocess.py
+++ EALR/dataprocess.py
@@ -11,6 +11,14 @@ def readfiles(projects):
         print(my_matrix.tail(5))
     return my_matrix
 
+#Series转list后去重
+def quchong(list):
+    ym=[]
+    for i in list:
+        if i not in ym:
+            ym.append(i)
+    return(ym)
+
 # def divedeTosubsets(data,folds,seed):
 #     # seed使得结果可复现，打乱现有顺序
 #     np.random.seed(seed)
@@ -20,12 +28,39 @@ def readfiles(projects):
 #     return sub
 
 if __name__ == '__main__':
-    projects=['jdt.csv']
-    # data = readfiles(projects)
-    data = pd.read_csv('../dataset2/jdt.csv', index_col='commitdate', parse_dates=True).sort_index()
-    print(data.tail(5))
-    print("------------")
-    print(data.sort_index)
-    # for str in data['commitdate']:
-        # np.Series a = datetime.datetime.strptime(data['commitdate'],"%Y/%m/%d  %H:%M:%S")
+    # projects=['jdt.csv']
+    # # data = readfiles(projects)
+    # data = pd.read_csv('../dataset2/jdt.csv', index_col='commitdate', parse_dates=True).sort_index()
+    # print(data.tail(5))
+    # print("------------")
+    # print(data.sort_index)
+
+    df = pd.read_csv('../dataset2/jdt.csv', sep=",")
+    df['commitdate'] = pd.to_datetime(df['commitdate'], format='%Y/%m/%d %H:%M:%S')
+    df.set_index('commitdate',inplace=True)
+    df = df.sort_index()#按时间进行排序
+    # print(df['2001-09'])需要获取年月字符串组
+
+
+    print(type(df.index))
+    #获取所有的年月信息
+    pydate_array = df.index.to_pydatetime()
+    date_only_array = np.vectorize(lambda s: s.strftime('%Y-%m'))(pydate_array)
+    date_only_series = pd.Series(date_only_array)
+
+    ym = quchong(date_only_series)
+
+    sub=[]
+    for i in range(len(ym)):
+        sub.append(df[ym[i]])
+
+    fit = sub[0].append(sub[1])
+    est = sub[4].append(sub[5])
+
+
+
+
+
+
+
 
